ddb679b82a0afdc1ac5895010e61126c07f7688e
Louise White <louise.white30@nhs.net>
2020-07-17 11:16:49 +0100

    new sprint folder

--- app/routes.js
+++ app/routes.js
@@ -29,6 +29,8 @@ router.use('/sprint4', require('./views/sprint4/_routes'));
 
 router.use('/sprint5', require('./views/sprint5/_routes'));
 
+router.use('/sprint6', require('./views/sprint6/_routes'));
+
 // current sprint, remember to add older sprint when adding a new folder!
 router.use('/current', require('./views/current/_routes'));
 
